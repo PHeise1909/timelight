2214a2e7b2aa681bf2ce523374eebdf0ec8a81fe
philipp.heise <philipp.heise@student.uni-luebeck.de>
2023-12-08 09:46:14 +0100

    configured database connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
+require('dotenv').config()
+
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const connectDB = require('./config/database')
+
 const app = express()
 const PORT = 3500
 
-app.listen(PORT, () => {console.log(`listening on Port: ${PORT}`)})
+connectDB()
+
+mongoose.connection.once('open', () => {
+    console.log('DB connected')
+    app.listen(PORT, () => {console.log(`listening on Port: ${PORT}`)})
+})
