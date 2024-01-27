8f7564d2a2de30e21215dce7e3378e2dba8da5c1
Isabel Kulbach <isabel.kulbach@student.uni-luebeck.de>
2024-01-27 17:25:52 +0100

    Added Confetti to the final page. All questions have answers now

--- client/src/Presentation.vue
+++ client/src/Presentation.vue
@@ -4,15 +4,44 @@
         <Timer :key="timerKey"></Timer>
         <QuestionDisplay :initialQuestion="socketValue"></QuestionDisplay>
     </div> 
-    <div class="frage1" v-if="socketValue === 10">
-      Hallo
+    <div class="frage-allgemein frage1" v-if="socketValue === 10">
+      80s | 90s | 00s 
     </div>
-    <div class="frage2 question-grid" v-if="socketValue === 11">
+    <div class="frage-allgemein frage2" v-if="socketValue === 11">
       Eine Galápagos-Riesenschildkröte würde für die Strecke 
        <br> <div style="font-size: 3em;">20 Jahre</div> </br> 
       <br>benötigen</br>
-      </div>
-</div>
+    </div>
+    <div class="frage-allgemein frage3" v-if="socketValue === 12">
+      Nokia | Backstein | Smartphone 
+    </div>
+    <div class="frage-allgemein frage4" v-if="socketValue === 13">
+      Gangnam Style
+    </div>
+    <div class="frage-allgemein frage5" v-if="socketValue === 14">
+      Im Durchschnitt 2 mal
+    </div>
+    <div class="frage-allgemein frage6" v-if="socketValue === 15">
+      Das Holstentor ist blabla jünger/älter als ihr
+    </div>
+    <div class="frage-allgemein frage7" v-if="socketValue === 16">
+      Nachteulen 
+    </div>
+    <div class="frage-allgemein frage8" v-if="socketValue === 17">
+      Radikal!
+    </div>
+    <div class="frage-allgemein frage9" v-if="socketValue === 18">
+      Zeitraffer
+    </div>
+    <div class="frage-allgemein frage10" v-if="socketValue === 19">
+      AI generiertes Bild
+    </div>
+    <div ref="celebrate" v-if="socketValue === 20">
+    </div>
+    <div class="frage-allgemein ende" v-if="socketValue === 20">
+      Eure Teilnahme war radikal!
+    </div>
+  </div>
   
 </template>
 
@@ -35,23 +64,26 @@
     width: 100%;
     height: 100%;
   }
-  .frage1{
-    display: flex;
-  }
 
-  .question-grid{
+  .frage-allgemein{
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     height:100%;
-  }
-  .frage2{
     color: white;
     font-family: 'Bebas Neue', sans-serif;
     text-align: center;
     font-size: 5em;
+  }
+  .frage1{
     display: flex;
+  }
+
+  .question-grid{
+  }
+  .frage2{
+  
   }
 
 </style>
@@ -59,6 +91,7 @@
 <script>
 import QuestionDisplay from './components/question-display.vue';
 import Timer from './components/BaseTimer.vue';
+import confetti from "canvas-confetti";
 
 export default {
   components: {
@@ -70,11 +103,21 @@ export default {
       timerKey: 0,
       socketValue: 0,
       socket: null,
+      isCelebrate: false,
     };
   },
   created() {
    this.setupWebSocket();
   },
+
+  watch: {
+    socketValue(newValue) {
+      if (newValue === 20) {
+        this.celebrate();
+      }
+    },
+  },
+
   methods: {
     setupWebSocket() {
       this.socket = new WebSocket('ws://localhost:3500');
@@ -83,6 +126,16 @@ export default {
         if(data.type === 'update'){
           this.socketValue = data.value;
         }
+      });
+    },
+    celebrate() {
+      this.isCelebrate = true;
+      confetti({
+        particleCount: 300,
+        spread: 950,
+        origin: { y: 0.4 },
+        colors: ["#4E8DEA", "#D833E0", "#383991"],
+        disableForReducedMotion: true,
       });
     },
   },
