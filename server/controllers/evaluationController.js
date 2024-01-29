36fb998cb6d1e35c951bffb1f880f3b5fabbde84
philipp.heise <philipp.heise@student.uni-luebeck.de>
2024-01-29 17:16:06 +0100

    added Evaluation for question 1

--- server/controllers/evaluationController.js
+++ server/controllers/evaluationController.js
@@ -1,9 +1,58 @@
 const User = require('../models/user')
-const mongoose = require('mongoose');
+const {getSharedValue} = require('../index');
 
-evaluateQuestion0 = async(answersBlue, answersRed, answersGreen) => {
-    console.log(answersGreen);
-    return answersGreen;
+const countAnswers = (answers) => {
+    return answers.reduce((acc, answer) => {
+        acc[answer] = (acc[answer] || 0) + 1;
+        return acc;
+    }, {});
+}
+
+const getMostCommonAnswer = (answers) => {
+    const countMap = countAnswers(answers);
+    let mostCommonAnswer = '';
+    let maxCount = 0;
+    for(const [answer, count] of Object.entries(countMap)){
+        if(count > maxCount) {
+            mostCommonAnswer = answer;
+            maxCount = count;
+        }
+    }
+    return mostCommonAnswer;
+}
+
+const countMatchingAnswers = (answers, mostCommonAnswer) => {
+    const totalAnswers = answers.length;
+    const matchingAnswers = answers.filter(answer => answer === mostCommonAnswer).length;
+    return (matchingAnswers/totalAnswers)*100;
+}
+
+evaluateQuestionMusic = async(answersBlue, answersRed, answersGreen) => {
+
+    const mostCommonBlue = getMostCommonAnswer(answersBlue);
+    const mostCommonRed = getMostCommonAnswer(answersRed);
+    const mostCommonGreen = getMostCommonAnswer(answersGreen);
+
+    const percentageMatchingBlue = countMatchingAnswers(answersBlue, mostCommonBlue);
+    const percentageMatchingRed = countMatchingAnswers(answersRed, mostCommonRed);
+    const percentageMatchingGreen = countMatchingAnswers(answersGreen, mostCommonGreen);
+
+    const result = {
+        blue: {
+            answer: mostCommonBlue,
+            percentage: percentageMatchingBlue
+        },
+        red: {
+            answer: mostCommonRed,
+            percentage: percentageMatchingRed
+        },
+        green: {
+            answer: mostCommonGreen,
+            percentage: percentageMatchingGreen
+        }
+    }
+    console.log(result);
+    return result;
 }
 
 const getEvaluation = async (req, res) => {
@@ -22,7 +71,11 @@ const getEvaluation = async (req, res) => {
         const answersGreen = usersGreen.map(user => user.answers[questionIndex]);
         const answersRed = usersRed.map(user => user.answers[questionIndex]);
         
-        const answerReturn = evaluateQuestion0(answersBlue, answersRed, answersGreen);
+        const answerReturn = evaluateQuestionMusic(answersBlue, answersRed, answersGreen);;
+
+        if(getSharedValue == 10){
+            answerReturn = evaluateQuestionMusic(answersBlue, answersRed, answersGreen);
+        }
         res.status(200).json({
             answerReturn
         });
