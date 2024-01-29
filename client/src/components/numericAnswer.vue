f0c5be1f4f6d4d1f0a1f82011b1efdac4f269568
Mette Röhling <mette.roehling@student.uni-luebeck.de>
2024-01-29 22:06:19 +0100

    added Styling to Input-field

--- client/src/components/numericAnswer.vue
+++ client/src/components/numericAnswer.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="numeric-answer-container">
-      <div class="question">{{ question }}</div>
-      <div class="input-container">
-        <input type="number" v-model="numericAnswer" placeholder="Antwort eingeben" />
-      </div>
-      <button @click="sendNumericAnswer">Senden</button>
+  <div class="numeric-answer-container">
+    <div class="question">{{ question }}</div>
+    <div class="input-container">
+      <input type="number" v-model="numericAnswer" placeholder="Antwort eingeben" />
     </div>
-  </template>
+    <button @click="sendNumericAnswer">Senden</button>
+  </div>
+</template>
   
   <style scoped>
     .numeric-answer-container {
       display: flex;
       flex-direction: column;
       align-items: center;
-      justify-content: center;
+      justify-content: space-around;
       color: white;
       margin-top: 20px;
     }
   
     .question {
       position: absolute;
-      top: 1%;
+      top: 2%;
       left: 5%;
       right: 5%;
       color: white;
@@ -31,29 +31,36 @@
     }
   
     .input-container {
+      position: relative;
       background-color: white;
       width: 80%;
       padding: 10px;
-      border-radius: 5px;
       margin: 10px;
       display: flex;
       justify-content: center;
       align-items: center;
+      border-radius: 0%;
+      border-style: solid;
+      border-width: 0.8cm;
+      border-color: rgb(59, 33, 118);
     }
-  
+
     input {
       width: 100%;
       border: none;
       padding: 5px;
       box-sizing: border-box;
     }
-  
+
     button {
       background-color: rgb(79, 199, 78);
       color: white;
       padding: 10px;
       cursor: pointer;
       margin-top: 10px;
+      font-family: 'Be Vietnam', sans-serif;
+      text-align: center;
+      font-size: x-large;
     }
   </style>
   
